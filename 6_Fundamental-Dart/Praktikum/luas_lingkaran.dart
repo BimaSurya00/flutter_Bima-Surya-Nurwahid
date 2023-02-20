c3e0efa3a7e7cd79782b0359dac646ec67611d34
Bima Surya Nurwahid <bimasurya729@gmail.com>
2023-02-20 21:34:01 +0700

    Create luas_lingkaran.dart

---
+++ 6_Fundamental-Dart/Praktikum/luas_lingkaran.dart
@@ -0,0 +1,11 @@
+void main() {
+ //luas lingkaran 
+  double phi = 3.14;
+  int r = 7;
+  double luas = phi * r * r;
+  print(luas);
+  
+ //keliling lingkaran
+  double keliling = 2 * phi * r;
+  print(keliling);
+}
